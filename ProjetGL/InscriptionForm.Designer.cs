37ffb054dbc38f6ebc8b6bea406ef14909f0eb9c
Amandine Vallée <63154381+avallee002@users.noreply.github.com>
2020-12-12 18:13:24 +0100

    Visuel forms connexion/inscription

---
+++ ProjetGL/InscriptionForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ProjetGL
+{
+    partial class InscriptionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNewPassword2 = new System.Windows.Forms.TextBox();
+            this.tbNewPassword = new System.Windows.Forms.TextBox();
+            this.tbNewLogin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identifiant";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mot de passe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Confirmation du\r\nmot de passe";
+            // 
+            // tbNewPassword2
+            // 
+            this.tbNewPassword2.Location = new System.Drawing.Point(146, 180);
+            this.tbNewPassword2.Name = "tbNewPassword2";
+            this.tbNewPassword2.Size = new System.Drawing.Size(192, 20);
+            this.tbNewPassword2.TabIndex = 3;
+            this.tbNewPassword2.UseSystemPasswordChar = true;
+            // 
+            // tbNewPassword
+            // 
+            this.tbNewPassword.Location = new System.Drawing.Point(146, 128);
+            this.tbNewPassword.Name = "tbNewPassword";
+            this.tbNewPassword.Size = new System.Drawing.Size(192, 20);
+            this.tbNewPassword.TabIndex = 4;
+            this.tbNewPassword.UseSystemPasswordChar = true;
+            // 
+            // tbNewLogin
+            // 
+            this.tbNewLogin.Location = new System.Drawing.Point(146, 75);
+            this.tbNewLogin.Name = "tbNewLogin";
+            this.tbNewLogin.Size = new System.Drawing.Size(192, 20);
+            this.tbNewLogin.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(79, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Annuler";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button2.Location = new System.Drawing.Point(227, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Confirmer";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // InscriptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 307);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbNewLogin);
+            this.Controls.Add(this.tbNewPassword);
+            this.Controls.Add(this.tbNewPassword2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "InscriptionForm";
+            this.Text = "Inscription à la BDThèque";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbNewPassword2;
+        private System.Windows.Forms.TextBox tbNewPassword;
+        private System.Windows.Forms.TextBox tbNewLogin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ ProjetGL/InscriptionForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetGL
+{
+    public partial class InscriptionForm : Form
+    {
+        public InscriptionForm()
+        {
+            InitializeComponent();
+        }
+
+        public string NewLogin
+        {
+            get { return tbNewLogin.Text; }
+            set { tbNewLogin.Text = value; }
+        }
+
+        public string NewPassword
+        {
+            get { return tbNewPassword.Text; }
+            set { tbNewPassword.Text = value; }
+        }
+    }
+}
