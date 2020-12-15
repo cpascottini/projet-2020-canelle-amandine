0df221a5fc3e7f6a56e86e474238f134db9d1b83
cpascottini <cpascottini@ensc.fr>
2020-12-15 10:07:45 +0100

    classes + mapping (pb)

---
+++ Domain/Personne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public abstract class Personne
+    {
+        public virtual int Id { get; set; }
+        public virtual string Login { get; set; }
+        public virtual string Mdp { get; set; }
+
+        public Personne() { }
+        public Personne(int id, string login, string mdp)
+        {
+            Id = id;
+            Login = login;
+            Mdp = mdp;
+        }
+        public override string ToString()
+        {
+            return Login;
+        }
+    }
+}
